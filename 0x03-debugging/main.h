18c473672967ca3fb1fb84f3c1faf11342dbb2e3
Osakpolor Courage Ihensekhien <osascourage27@gmail.com>
2022-04-15 10:57:55 -0700

    multiple debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,7 +5,7 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-void convert_day(int month, int day);
 int print_remaining_days(int month, int day, int year);
+void convert_day(int month, int day);
 
 #endif /* MAIN_H */
